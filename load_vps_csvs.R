e5c0f22a327026601e97ddc5d26b3654b443f917
Brittney Scannell <brittney.scannell@stonybrook.edu>
2023-11-27 16:24:56 -0500

    removed example

--- load_vps_csvs.R
+++ load_vps_csvs.R
@@ -63,11 +63,3 @@ load_vps_files <- function() {
     
     return(combined_data)
   }
-
-  
-# Example usage:
-  
-  # call the function from github
-source("https://raw.githubusercontent.com/bjscannell/lab_code/master/load_vps_csvs.R")
-
-combined_data <- load_vps_files("detections")  
