3b0039da891a6807d635318b75ad5c565e3f399c
Brittney Scannell <brittney.scannell@stonybrook.edu>
2023-11-27 16:25:21 -0500

    removed example

--- load_vue_csvs.R
+++ load_vue_csvs.R
@@ -41,10 +41,3 @@ load_vue_files <- function(project) {
   
 }
 
-
-# Example usage:
-
-#load the function from github
-source("https://raw.githubusercontent.com/bjscannell/lab_code/master/load_vue_csvs.R")
-
-combined_data <- load_vue_files()
