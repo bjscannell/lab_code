192a116df9960dd91327af9945b6bd22c053e5b8
Brittney Scannell <brittney.scannell@stonybrook.edu>
2024-07-05 12:30:29 -0400

    I fucking forgot everything needs to be in UTC

--- Updating_dualID_tagsMetadata.R
+++ Updating_dualID_tagsMetadata.R
@@ -3,6 +3,7 @@
 ## Code for pulling dual sensor tag info
 library(readxl)
 library(tidyverse)
+library(lubridate)
 
 # Set working directory to where you have dumped the download of zipped files
 setwd("/Users/brittneyscannell/Desktop")
@@ -24,6 +25,8 @@ sharks <- read_excel("/Users/brittneyscannell/Desktop/Shark_Capture.xlsx")
 sn <- sharks %>%
   pull(`Acoustic Serial Number`)
 
-
+# Join with shark info
+## Don't forget everythings in fucking UTC
 sharks %>% left_join(tags, by = c("Acoustic Serial Number" = "Serial No.")) %>% 
+  mutate(Hooked_UTC = with_tz(ymd_hms(Hooked), tzone = "UTC")) %>% 
   write_csv("matos_tagupdate.csv")
