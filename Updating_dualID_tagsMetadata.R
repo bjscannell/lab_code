3a0f1b4a96c6c93e80350bc2aeab4a1b734ec0ab
Brittney Scannell <brittney.scannell@stonybrook.edu>
2024-07-24 11:11:54 -0400

    adding other otn metadata columns

--- Updating_dualID_tagsMetadata.R
+++ Updating_dualID_tagsMetadata.R
@@ -32,7 +32,34 @@ sn <- sharks %>%
 sharks %>% left_join(tags, by = c("Acoustic Serial Number" = "Serial No.")) %>% 
   mutate(Code_ID = str_sub(`VUE Tag ID`, 10),
          CodeSpace = str_sub(`VUE Tag ID`, 1,8),
-         Sex = str_sub(Sex,1,1)) %>% 
-  select(`Field ID`,`Acoustic Serial Number`, Code_ID, CodeSpace, 
-         Tagger, Species, Location,TL, FL, Sex, y, x, Hooked) %>% 
+         Sex = str_sub(Sex,1,1),
+         Tag_Type = "Acoustic",
+         Manufacturer = "Vemco",
+         Implant_type = "Internal",
+         Method = "",
+         Activation = "",
+         Tag_Life = 3650,
+         Owner = "Bradley Peterson",
+         Ogranization = "Stony Brook Universtiy",
+         Scientific_Name = "",
+         Cap_Lat = "",
+         Cap_Lon = "", 
+         Wild = "W",
+         Stock = "Atlantic",
+         Weight = "",
+         Length_type = "TOTAL",
+         length2 = "",
+         LENGTH_TYPE2 = "",
+         Life_stage = "",
+         age = "",
+         age_units = "",
+         dna = "Y",
+         treatment_type = "",
+         release_group = "",
+         time_zone = "UTC") %>% 
+  select(`Field ID`,Tag_Type,Manufacturer, `Acoustic Serial Number`, Code_ID, CodeSpace, 
+         Implant_type, Method, Activation, Tag_Life, Tagger, Owner, Ogranization, Species, 
+         Scientific_Name, Location, Cap_Lat, Cap_Lon, Wild, Stock, TL, Weight, Length_type,
+         length2, LENGTH_TYPE2, Life_stage, age, age_unit, Sex, dna, treatment_type, release_group,
+         Location, y, x, Hooked, time_zone) %>% 
   write_csv("matos_tagupdate.csv")
